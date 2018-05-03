d966630b3706354a3b7cd6850d85d7f390e5f7ff
Michoel Snow <msnow1@montefiore.org>
2018-05-03 08:18:30 -0400

    added more imports

--- src/features/imports.py
+++ src/features/imports.py
@@ -1,7 +1,10 @@
-import IPython, graphviz, sklearn_pandas, sklearn, warnings, re
+import IPython, graphviz, sklearn_pandas, sklearn, warnings, re, scipy
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 from sklearn import metrics
 from concurrent.futures import ProcessPoolExecutor
-
+from scipy.cluster import hierarchy as hc
+import seaborn as sns; sns.set(color_codes=True)
+from pdpbox import pdp
+from plotnine import *
