87366e4118c6ae55c8e3777e34b7b8b064a1c19a
Michoel Snow <protect3f@gmail.com>
2018-05-26 12:27:38 -0400

    changed directories

---
+++ src/data_sci/imports.py
@@ -0,0 +1,19 @@
+import IPython, graphviz, sklearn_pandas, sklearn, warnings, re, scipy, plotnine, math, os, pickle, gzip, cv2, PIL
+import collections
+import threading
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn import metrics
+from concurrent.futures import ProcessPoolExecutor
+from scipy.cluster import hierarchy as hc
+import seaborn as sns; sns.set(color_codes=True)
+from pdpbox import pdp
+from plotnine import *
+from tqdm import tqdm, tqdm_notebook, tnrange
+from abc import abstractmethod
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+from PIL import Image, ImageEnhance, ImageOps
+from glob import glob, iglob
+from distutils.version import LooseVersion
+from collections import Iterable, Counter, OrderedDict
