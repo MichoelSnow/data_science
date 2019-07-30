af75f2ffa9fde2e1d8059b0ad8421c4bcb62deb5
Michoel Snow <protect3f@gmail.com>
2019-07-30 08:53:42 -0400

    added metrics for kaggle planet

--- src/data_sci/fastai_dl_course.py
+++ src/data_sci/fastai_dl_course.py
@@ -1,4 +1,4 @@
-import os, collections, cv2, torch, math, threading, random, copy, bcolz, contextlib
+import os, collections, cv2, torch, math, threading, random, copy, bcolz, contextlib, warnings
 import numpy as np
 from tqdm import tqdm, tqdm_notebook, tnrange
 from glob import glob
@@ -13,6 +13,7 @@ from collections import Iterable, OrderedDict
 from itertools import chain, product
 from distutils.version import LooseVersion
 import matplotlib.pyplot as plt
+from sklearn.metrics import fbeta_score
 
 from torch import nn, optim
 from torch.autograd import Variable
@@ -3691,3 +3692,33 @@ def plots_raw(ims, figsize=(12, 6), rows=1, titles=None):
 
 def load_img_id(ds, idx, path):
     return np.array(Image.open(os.path.join(path, ds.fnames[idx])))
+
+
+#################################
+#  PLANET
+
+def f2(preds, targs, start=0.17, end=0.24, step=0.01):
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        return max([fbeta_score(targs, (preds>th), 2, average='samples')
+                    for th in np.arange(start,end,step)])
+
+def opt_th(preds, targs, start=0.17, end=0.24, step=0.01):
+    ths = np.arange(start,end,step)
+    idx = np.argmax([fbeta_score(targs, (preds>th), 2, average='samples')
+                for th in ths])
+    return ths[idx]
+
+def get_data(path, tfms,bs,  n, cv_idx):
+    val_idxs = get_cv_idxs(n, cv_idx)
+    return ImageClassifierData.from_csv(path, 'train-jpg', f'{path}train_v2.csv', bs, tfms,
+                                 suffix='.jpg', val_idxs=val_idxs, test_name='test-jpg')
+
+def get_data_zoom(f_model, path, sz, bs, n, cv_idx):
+    tfms = tfms_from_model(f_model, sz, aug_tfms=transforms_top_down, max_zoom=1.05)
+    return get_data(path, tfms, bs, n, cv_idx)
+
+def get_data_pad(f_model, path, sz, bs, n, cv_idx):
+    transforms_pt = [RandomRotateZoom(9, 0.18, 0.1), RandomLighting(0.05, 0.1), RandomDihedral()]
+    tfms = tfms_from_model(f_model, sz, aug_tfms=transforms_pt, pad=sz//12)
+    return get_data(path, tfms, bs, n, cv_idx)
